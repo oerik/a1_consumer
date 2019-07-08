94e55ff2a500e15116286860cc9dd968769797e6
erik <oomen.e@gmail.com>
2019-07-08 14:25:33 +0000

    Use producer group and use broker timestamp

--- consumer.go
+++ consumer.go
@@ -23,7 +23,7 @@ func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
 }
 
 func main() {
-    kafkaReader := getKafkaReader("192.168.1.106:9092", "sensors", "")
+    kafkaReader := getKafkaReader("192.168.1.106:9092", "sensors", "group1")
     defer kafkaReader.Close()
     httpClient, err := client.NewHTTPClient(client.HTTPConfig{
         Addr: "http://localhost:8086",
@@ -48,6 +48,7 @@ func main() {
 		})
                 json.Unmarshal([]byte(m.Value),  &result)
 		tags := map[string]string {"sensor": result["Device_ID"].(string)}
+		timestamp := int64(result["timestamp"].(float64))
 		fields := map[string]interface{}{
 			"temp": result["temp"].(float64),
 			"hum": result["hum"].(float64),
@@ -58,14 +59,15 @@ func main() {
 			"z": result["z"].(float64),
 			"db": result["db"].(float64),
 			"button": int(result["button"].(float64)),
+			"timestamp": timestamp,
 		}
-		pt, err := client.NewPoint(result["Device_ID"].(string), tags, fields, m.Time)
+		pt, err := client.NewPoint(result["Device_ID"].(string), tags, fields, time.Unix(timestamp, 0))
                 if err != nil {
                     fmt.Println("Error: ", err.Error())
                 }
                 bp.AddPoint(pt)
 		httpClient.Write(bp)
-		fmt.Printf("message: %v\n", result["button"])
+		fmt.Printf("message: %v %v\n", result["button"], fields)
 
 	}
 }
